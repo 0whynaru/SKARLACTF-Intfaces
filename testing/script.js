9cf0a61c911d9fa6d01f678a6509dfd7c1559cdd
0whyn <taparnalah@gmail.com>
2026-05-22 11:26:00 +0800

    ADD ALL

---
+++ testing/script.js
@@ -0,0 +1,8 @@
+let x = prompt('SaldoAwal: ')
+let y = prompt('SaldoTambahan: ')
+const saldoakhir = Number(x) + Number(y)
+alert('Saldo Akhir mu adalah: ' + saldoakhir)
+let user = prompt('Masukan perintah: ')
+if (user == saldo) {
+    alert(saldoakhir)
+}
